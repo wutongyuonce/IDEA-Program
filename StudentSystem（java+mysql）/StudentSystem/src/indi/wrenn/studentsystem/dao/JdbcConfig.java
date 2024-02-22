f769af5fd729b2bf9dc3ed207276eac7486db09f
wutongyuonce <3304984553@qq.com>
2024-02-22 22:15:51 +0800

    student system

---
+++ StudentSystem（java+mysql）/StudentSystem/src/indi/wrenn/studentsystem/dao/JdbcConfig.java
@@ -0,0 +1,8 @@
+package indi.wrenn.studentsystem.dao;
+
+public interface JdbcConfig {
+    String DRIVER = "com.mysql.cj.jdbc.Driver";
+    String URL = "jdbc:mysql://localhost:3306/studentsystemdao";
+    String USERNAME = "root";
+    String PASSWORD = "Zhang20050730_";
+}
