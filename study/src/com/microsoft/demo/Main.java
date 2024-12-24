9057dd13ff6d42241ab2445ce661202c84b5482b
wutongyuonce <3304984553@qq.com>
2024-12-24 21:07:26 +0800

    1

---
+++ study/src/com/microsoft/demo/Main.java
@@ -0,0 +1,18 @@
+package ; //定位你的Java代码放在src文件夹下的位置
+
+//公共的  类   类名（开头字母必须大写和文件名相同）
+public class Main{
+
+    //mian函数的固定格式
+    public static void main(String[] args){
+
+        //语句
+        System.out.println("Hello");
+    }
+
+    //java函数
+    public static int sum(int number_a,int number_b){
+        return number_a+number_b
+    }
+    //函数的调用和C是一样的
+}
---
+++ study/src/com/microsoft/demo/student.java
@@ -0,0 +1,2 @@
+package com.microsoft.demo;public class student {
+}
