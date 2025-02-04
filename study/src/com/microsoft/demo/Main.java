700a4d9f84c195302221bef52fc62d904d910a08
YuOnce <3304984553@qq.com>
2025-02-05 00:46:46 +0800

    1

--- study/src/com/microsoft/demo/Main.java
+++ study/src/com/microsoft/demo/Main.java
@@ -1,12 +1,12 @@
 package com.microsoft.demo; //定位你的Java代码放在src文件夹下的位置
 
-/**
- * @author jackson
- * @since 1.8
- * @version 1.0
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Objects;
+
 public class Main{
-    //main函数的固定格式
     public static void main(String[] args) {
+        
     }
 }
